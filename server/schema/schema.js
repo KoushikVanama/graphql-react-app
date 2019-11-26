1e9c99240006c144324f913496b67db5e309da79
sai koushik <saikoushik@vsvk.local>
2019-11-26 22:14:09 +0530

    simulating data using graphiql

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,21 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const _ = require('lodash');
+
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+/*
+ * Responsibilites of schema:
+ * 1. Define types
+ * 2. define relationships btw types
+ * 3. Root queries
+ **/
+
+ const books = [
+   { id: "1" ,genre: 'Action', name: "Name1"},
+   { id: "2" ,genre: 'Horror', name: "Name2"},
+   { id: "3" ,genre: 'Sci-fi', name: "Name3"}
+ ];
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
@@ -8,5 +23,23 @@ const BookType = new GraphQLObjectType({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
     genre: { type: GraphQLString }
-  });
+  })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+      book: {
+        type: BookType,
+        args: { id: { type: GraphQLString }},
+        resolve(parent, args){
+          // code to get data from db/other source
+          return _.find(books, {id: args.id})
+        }
+      }
+    }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 });
