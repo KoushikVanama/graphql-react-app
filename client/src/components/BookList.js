038c87baeab6422723a082009ddf23118f555cbb
sai koushik <saikoushik@vsvk.local>
2019-11-27 22:56:35 +0530

    adding react setup

---
+++ client/src/components/BookList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class BookList extends React.Component{
+  render(){
+    return (
+      <ul id="book-list">
+        <li>Book Name</li>
+      </ul>
+    );
+  }
+}
+
+export default BookList;
