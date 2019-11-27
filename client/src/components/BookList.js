ae1288eb193d655826258b2edd5f9cf59ead639f
sai koushik <saikoushik@vsvk.local>
2019-11-27 23:21:09 +0530

    apollo client setup and queries from react

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { gql } from 'apollo-boost';
+import { graphql } from 'react-apollo';
+
+const getBooksQuery = gql`
+  {
+    books{
+      name
+      id
+    }
+  }
+`;
 
 class BookList extends React.Component{
   render(){
+    console.log(this.props);
     return (
       <ul id="book-list">
         <li>Book Name</li>
@@ -10,4 +22,4 @@ class BookList extends React.Component{
   }
 }
 
-export default BookList;
+export default graphql(getBooksQuery)(BookList);
--- server/app.js
+++ server/app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 mongoose.connect('mongodb://sai:sai1234@ds053539.mlab.com:53539/graphql-project');
 mongoose.connection.once('open', () =>{
